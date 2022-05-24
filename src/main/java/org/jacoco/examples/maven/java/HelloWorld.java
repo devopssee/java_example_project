3381399808440c3d3813168c17a7f6db914bd6aa
devopssee <devops.seemaladari@gmail.com>
2022-05-24 04:54:40 +0000

    Changed the print stmnt in main/java/org/jacoco/examples/maven/java/HelloWorld.java

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		if (bigger) {
 			return "Hello Universe!";
 		} else {
-			return "Hello Ashok, Welcome to the World!";
+			return "Hello Ashok, Glad to see you";
 		}
 	}
 
